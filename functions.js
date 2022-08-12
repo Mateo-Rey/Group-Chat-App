f9f67e1f51d445d3800a11472083ab5bc3a048d4
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-12 11:20:31 -0400

    functions for post and get requests

---
+++ functions.js
@@ -0,0 +1,30 @@
+function handleError(err, res) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+  
+  export function getMessage(req, res) {
+    const db = dbConnect();
+    db.collection("messages")
+      .get()
+      .then((collection) => {
+        const messages = collection.docs.map((doc) => doc.data());
+        res.send(messages);
+      })
+      .catch((err) => handleError(err, res));
+  }
+  
+  export function createMessage(req, res) {
+    const newMessage = req.body;
+    const db = dbConnect();
+    db.collection("messages")
+      .add(newMessage)
+      .then((doc) => {
+        res.status(201).send({
+          success: true,
+          id: doc.id,
+        });
+      })
+      .catch((err) => handleError(err, res));
+  }
+  
